cf55f8cbc55d5a55f41274ef1880dd68fdc61d8b
Mayya Matueva <mayamatueva@gmail.com>
2025-08-11 13:58:38 +0200

    resize the image so that the smaller side is 256 pixels

--- preprocessing.py
+++ preprocessing.py
@@ -11,3 +11,13 @@ plt.imshow(image)
 plt.title("Original Image")
 plt.axis("off")  # turn off axes for clarity
 plt.show()
+
+# 2. Resize the image so that the smaller side is 256 pixels
+resize_transform = transforms.Resize(256)
+resized_image = resize_transform(image)
+print("Image size after Resize:", resized_image.size)
+
+plt.imshow(resized_image)
+plt.title("Image after Resize")
+plt.axis("off")
+plt.show()
